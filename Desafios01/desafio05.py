cd18a5e4d3085f12352a4b1a1bf3ef8bb192089c
JoaoLCaracciolo <caracciolojl@hotmail.com>
2023-10-04 21:39:48 -0300

    Desafios01

--- Desafios01/desafio05.py
+++ Desafios01/desafio05.py
@@ -7,4 +7,31 @@ vetor;
 vetor;
 (3) quantos dos valores do vetor são
 maiores que a média desses valores:"""
+numeros = [0]*30
+contador = 0
+maiorNum = 0
+soma = 0
 
+for i in range(30):
+    numeros[i] = int(input(f"Digite o {i+1}° numero:"))
+    soma += numeros[i]
+
+media = soma/30
+menorNum = numeros[0]
+
+print("-----Numeros Pares-----")
+for o in range(30):
+    if numeros[o] % 2 == 0:
+        print(numeros[o], end="-")
+
+for p in range(30):
+    if numeros[p] > maiorNum:
+        maiorNum = numeros[p]
+    if numeros[p] < menorNum:
+        menorNum = numeros[p]
+
+for k in range(30):
+    if numeros[k] > media:
+        contador += 1
+
+print(f"\nMaior numero:{maiorNum}\nMenor Numero:{menorNum}\nNumeros Maiores que a media:{contador}")
